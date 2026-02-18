b37084e4ef4fa9256cae4a793377ddbdd6635d9d
vsegersall <viktor.segersall@proton.me>
2026-02-18 12:18:38 +0100

    Basic testing of S4 methods using addition. Should be implemented lazily.

--- R/LazyMatrix-class.R
+++ R/LazyMatrix-class.R
@@ -16,7 +16,7 @@ setClass("LazyMatrix",
 # users to only call LazyMatrix(X) without "operations"
 LazyMatrix <- function(data, operations=list()){
   # code for constructing helper
-  # new("LazyMatrix", data=data, operations=operations)
+  new("LazyMatrix", data=data, operations=operations)
 }
 
 # Validity checks on the arguments
@@ -27,3 +27,30 @@ setValidity("LazyMatrix", function(object){
 })
 
 # Methods to the class
+## Addition
+### Generic
+setGeneric("addition", function(x, y) standardGeneric("addition"))
+
+### two regular R matrices
+setMethod("addition", c("matrix", "matrix"), function(x, y){
+  newmat <- matrix(NA, ncol=ncol(x),
+                   nrow=nrow(x))
+  for (i in 1:nrow(x)){
+    for (j in 1:ncol(x)){
+      newmat[i,j] = x[i,j] + y[i, j]
+    }
+  }
+  newLazy <- LazyMatrix(newmat)
+})
+
+### two lazy matrices
+setMethod("addition", c("LazyMatrix", "LazyMatrix"), function(x, y){
+  newmat <- matrix(NA, ncol=ncol(x@data),
+                   nrow=nrow(x@data))
+  for (i in 1:nrow(x@data)){
+    for (j in 1:ncol(x@data)){
+      newmat[i, j] <- x@data[i, j] + y@data[i, j]
+    }
+  }
+  newLazy <- LazyMatrix(newmat)
+  })
--- tests/testthat/test-LazyMatrix-class.R
+++ tests/testthat/test-LazyMatrix-class.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Basic lazy matrix operation work! ", {
+  # 1. Create 2 regular matrices
+  mat.a <- matrix(1:4, 2, 2)
+  mat.b <- matrix(5:8, 2, 2)
+
+  # 2. Create 2 lazy matrices
+  a <- LazyMatrix(mat.a)
+  b <- LazyMatrix(mat.b)
+
+  # 3. Addition
+  c <- addition(a, b)
+  expect_s4_class(c, "LazyMatrix")
+  expect_equal(c@data, mat.a + mat.b)
+  #expect_equal(lazy.subtraction(a, b), mat.a - mat.b)
 })
